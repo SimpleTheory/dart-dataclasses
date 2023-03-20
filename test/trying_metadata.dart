7109536db4f13e63ce05aeecce00f078bc5d66b1
Ari Frid <arigfrid@hotmail.com>
2023-03-19 22:36:56 -0700

    sub update

--- test/trying_metadata.dart
+++ test/trying_metadata.dart
@@ -27,3 +27,45 @@ Map dejsonifyMap(Map map){
   return Map.from(map.map((key, value) =>
       MapEntry(dejsonify(key), dejsonify(value))));
 }
+// Serialize JSON
+
+jsonify(thing) {
+  try {
+    return thing.toMap();
+  } on NoSuchMethodError {
+    if (isJsonSafe(thing)) {
+      return thing;
+    } else if (supportedTypeToMap(thing) != null) {
+      return supportedTypeToMap(thing);
+    } else if (thing is Iterable && !isMap(thing)) {
+      return nestedJsonList(thing);
+    } else if (isMap(thing)) {
+      return nestedJsonMap(thing);
+    } else {
+      throw Exception('Error on handling $thing since ${thing.runtimeType} '
+          'is not a base class or does not have a toJson() method');
+    }
+  }
+}
+
+List nestedJsonList(Iterable iter) {
+  List l = [];
+  for (var thing in iter) {
+    l.add(jsonify(thing));
+  }
+  return l;
+}
+
+Map nestedJsonMap(mapLikeThing) {
+  Map m = {};
+  var key;
+  var value;
+
+  for (MapEntry mapEntry in mapLikeThing.entries) {
+    key = jsonify(mapEntry.key);
+    value = jsonify(mapEntry.value);
+    m[key] = value;
+  }
+
+  return m;
+}
